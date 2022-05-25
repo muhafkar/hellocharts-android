68e756cad104f6ea71601b26758f030aaabc9c82
Muhammad Afkar <afkar@learn-quran.co>
2022-05-25 22:57:12 +0700

    fix lagi biar bisa build

--- hellocharts-library/src/lecho/lib/hellocharts/view/hack/HackyViewPager.java
+++ hellocharts-library/src/lecho/lib/hellocharts/view/hack/HackyViewPager.java
@@ -1,9 +1,10 @@
 package lecho.lib.hellocharts.view.hack;
 
 import android.content.Context;
-import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+
+import androidx.viewpager.widget.ViewPager;
 
 /**
  * ScaleGestureDetector seems to mess up the touch events, which means that ViewGroups which make use of
